17fd79aa98807d9a8fbc84905f015c1fe7f8ef6a
Alexander Bauer <sasha@crofter.org>
2014-04-08 02:50:05 -0400

    Do major refactoring and add argparse

--- timesheet.py
+++ timesheet.py
@@ -1,67 +1,76 @@
 #!/usr/bin/python
 
-import sys
-from datetime import datetime
+import sys, argparse
+from datetime import datetime, date, time
+from os.path import expanduser
+
+FILEPATH = expanduser("~/.timesheets")
+EPOCH = date(1970, 1, 1)        # UNIX EPOCH
 
 class Timesheet:
-    def newweek(this, week):
-        this.weeks.append(week)
+    def newperiod(this, period):
+        this.workperiods.append(period)
+        if not period.iscomplete():
+            this.activeperiod = None
 
-    def __init__(this, weeks = []):
-        this.weeks = weeks
-
-class Week:
-    def newday(this, workday):
-        this.workdays.append(workday)
-
-    def sumtime(this):
-        # For each recorded weekday, sum the time and add it to the
-        # total.
-        total = 0
-        for workday in this.workdays:
-            total += workday.sumtime()
-        
-        return total
-
-    def __init__(this, year = None, week = None, workdays = []):
-        isocal = datetime.now().isocalendar() # (year, weeknum, weekday)
-        if year == None: year = isocal[0]     # Set the current year
-        if week == None: week = isocal[1]     # Set the current week day
-
-        this.year = year
-        this.week = week
-        this.workdays = workdays
-
-class Workday:
-    def newperiod(this, workperiod):
-        this.workperiods.append(workperiod)
-
-    def sumtime(this):
-        # For each set of times in and times out, sum their
-        # difference.
-        total = 0
-        for workperiod in this.workperiods:
-            total += workperiod.sumtime()
-
-        return total
-
-    def __init__(this, isoday = None, workperiods = []):
-        if isoday == None: isoday = datetime.now().isocalendar()[2]
-
-        this.isoday = isoday
+    def __init__(this, date, workperiods = []):
+        this.date = date
         this.workperiods = workperiods
+        this.activeperiod = None
 
 class Workperiod:
-    def sumtime(this):
-        # Return the difference of the times in and out.
-        return this.outtime - this.intime
+    def iscomplete(this):
+        return (this.timein != None and this.timeout != None)
 
-    def __init__(this, timein, timeout):
+    def sumtime(this):
+        # Return the difference of the times in and out. The times
+        # should be pure datetime.time objects, so we convert them to
+        # datetime objects starting from the UNIX Epoch.
+        return datetime.combine(EPOCH, this.outtime) \
+            - datetime.combine(EPOCH, this.intime)
+
+    def __init__(this, timein, timeout = None):
         this.timein = timein
         this.timeout = timeout
 
+@classmethod
+def parseWorkperiod(cls, string):
+    if "-" in string:
+        splitstring = string.split("-")
+        timein = datetime.strptime(splitstring[0], "%H:%M").time()
+        timeout = datetime.strptime(splitstring[1], "%H:%M").time()
+        return cls(timein, timeout)
+
+Workperiod.parse = parseWorkperiod
+
 def main(argc, argv):
-    pass
+    flags = parseflags(argv[1:])
+    
+
+
+def parseflags(args):
+    parser = argparse.ArgumentParser(description=
+                                     "Manage timesheets.")
+    # Get the file path if given.
+    parser.add_argument("--file", "-f", action="store",
+                        default=FILEPATH,
+                        help="path to timesheet directory")
+
+    # Get the department of work to be done.
+    parser.add_argument("--department", "-d", action="store",
+                        required=False, help="department or descriptor \
+                        to record time for")
+
+    # Determine whether the type is in or out.
+    parser.add_argument("check", action="store", choices=["in", "out"],
+                        help="check in or out")
+
+    # Get the time at which to check in or out.
+    parser.add_argument("time", action="store",
+                        default=datetime.now().strftime("%H:%M"),
+                        help="time to check in or out at")
+
+    return parser.parse_args(args)
 
 def promptdefault(prompt, default = None):
     # Attach some suffix to the end of the prompt. If the default is
